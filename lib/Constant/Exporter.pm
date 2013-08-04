2eeccfda64f22447e1f7b275c278e86985ee71a3
punytan <punytan@gmail.com>
2013-08-04 13:42:34 +0900

    better interface

--- lib/Constant/Exporter.pm
+++ lib/Constant/Exporter.pm
@@ -6,66 +6,54 @@ require Exporter;
 
 sub import {
     my ($class, %args) = @_;
-
-    my $pkg = caller;
-
     my (@export, @export_ok, %export_tags, %constants);
 
-    if ($args{EXPORT_TAGS}) {
-        $args{EXPORT}    ||= [];
-        $args{EXPORT_OK} ||= [];
-
-        unless (ref $args{EXPORT} eq 'ARRAY' && ref $args{EXPORT_OK} eq 'ARRAY') {
-            require Carp;
-            Carp::croak("EXPORT and EXPORT_OK expects ArrayRef");
-        }
-
-        @export    = @{$args{EXPORT}};
-        @export_ok = @{$args{EXPORT_OK}};
-
-        for my $tag (keys %{ $args{EXPORT_TAGS} }) {
-            for my $name (keys %{ $args{EXPORT_TAGS}{$tag} || {} }) {
-                my $value = $args{EXPORT_TAGS}{$tag}{$name};
-                $constants{$name} = $value;
-                $export_tags{$tag} ||= [];
+    for my $key (qw/ EXPORT_TAGS EXPORT_OK_TAGS /) {
+        for my $tag (keys %{ $args{$key} || {} }) {
+            $export_tags{$tag} ||= [];
+            for my $name (keys %{ $args{$key}->{$tag} || {} }) {
+                $constants{$name} = $args{$key}->{$tag}{$name};
                 push @{$export_tags{$tag}}, $name;
+
+                if ($key eq 'EXPORT_TAGS') {
+                    push @export, $name;
+                } elsif ($key eq 'EXPORT_OK_TAGS') {
+                    push @export_ok, $name;
+                }
+            }
+        }
+    }
+
+    for my $key (qw/ EXPORT EXPORT_OK /) {
+        for my $name (keys %{ $args{$key} || {} }) {
+            $constants{$name} = $args{$key}->{$name};
+            if ($key eq 'EXPORT') {
+                push @export, $name;
+            } elsif ($key eq 'EXPORT_OK') {
                 push @export_ok, $name;
             }
         }
-    } else {
-        $args{EXPORT}    ||= {};
-        $args{EXPORT_OK} ||= {};
-
-        unless (ref $args{EXPORT} eq 'HASH' && ref $args{EXPORT_OK} eq 'HASH') {
-            require Carp;
-            Carp::croak("EXPORT and EXPORT_OK expects HashRef");
-        }
-
-        for my $name (keys %{ $args{EXPORT} || {} }) {
-            $constants{$name} = $args{EXPORT}->{$name};
-            push @export, $name;
-        }
-        for my $name (keys %{ $args{EXPORT_OK} || {} }) {
-            $constants{$name} = $args{EXPORT_OK}->{$name};
-            push @export_ok, $name;
-        }
     }
 
-    no strict 'refs';
-    for my $name (keys %constants) {
-        my $value = $constants{$name};
-        *{"${pkg}::${name}"} = sub () { $value };
+    my $pkg = caller;
+
+    {
+        no strict 'refs';
+        for my $name (keys %constants) {
+            my $value = $constants{$name};
+            *{"${pkg}::${name}"} = sub () { $value };
+        }
+
+        push @{"${pkg}::ISA"}, 'Exporter';
+        push @{"${pkg}::EXPORT"}, @export;
+        push @{"${pkg}::EXPORT_OK"}, @export_ok;
+        %{"${pkg}::EXPORT_TAGS"} = %export_tags;
+
+        *{"${pkg}::import"} = sub {
+            my $pkg = shift;
+            $pkg->export_to_level(1, undef, @_)
+        };
     }
-
-    push @{"${pkg}::ISA"}, 'Exporter';
-    push @{"${pkg}::EXPORT"}, @export;
-    push @{"${pkg}::EXPORT_OK"}, @export_ok;
-    %{"${pkg}::EXPORT_TAGS"} = %export_tags;
-
-    *{"${pkg}::import"} = sub {
-        my $pkg = shift;
-        $pkg->export_to_level(1, undef, @_)
-    };
 }
 
 1;
@@ -75,15 +63,81 @@ __END__
 
 =head1 NAME
 
-Constant::Exporter - Blah blah blah
+Constant::Exporter - define and export constants easily
 
 =head1 SYNOPSIS
 
-  use Constant::Exporter;
+    # define constants in your MyApp::Constants,
+
+    package MyApp::Constants;
+    use strict;
+    use warnings;
+
+    use Constant::Exporter (
+        EXPORT => {
+            FB_CLIENT_ID => 12345,
+        },
+        EXPORT_OK => {
+            TITLE_MAX_LENGTH => 128,
+        },
+        EXPORT_TAGS => {
+            user_status => {
+                USER_STATUS_FB_ASSOCIATED     => 1,
+                USER_STATUS_FB_NOT_ASSOCIATED => 0,
+            },
+        },
+        EXPORT_OK_TAGS => {
+            fb_api_error => {
+                ERROR_OAUTH       => 190,
+                ERROR_API_SESSION => 102,
+                ERROR_API_USER_TOO_MANY_CALLS => 17,
+            },
+            fb_payment_error => {
+                ERROR_PAYMENTS_ASSOCIATION_FAILURE   => 1176,
+                ERROR_PAYMENTS_INSIDE_IOS_APP        => 1177,
+                ERROR_PAYMENTS_NOT_ENABLED_ON_MOBILE => 1178,
+            },
+        },
+    );
+
+    1;
+
+    # then use it like Exporter's `%EXPORT_TAGS` and `@EXPORT_OK`
+    package main;
+    use MyApp::Constants qw( TITLE_MAX_LENGTH :fb_api_error );
+
+    sub foo {
+        my ($title) = @_;
+        if (length $title > TITLE_MAX_LENGTH) {
+            ...
+        }
+    }
+
+    sub bar {
+        my ($response) = @_;
+
+        if ($response->{error}{code} == ERROR_OAUTH) {
+            ...
+        }
+    }
+
 
 =head1 DESCRIPTION
 
-Constant::Exporter is
+Constant::Exporter is a module to define and export constants easily.
+
+This module adopts L<Exporter>'s full functionality so you can import constants with default constants, tags or only selected constants.
+
+=head1 KEYS AND MEANINGS
+
+=head2 C<EXPORT>, C<EXPORT_TAGS>
+
+Constant names in C<EXPORT> and C<EXPORT_TAGS> will be exported by default.
+
+=head2 C<EXPORT_OK> C<EXPORT_OK_TAGS>
+
+Constant names in C<EXPORT_OK> and C<EXPORT_OK_TAGS> will not be exported by default.
+You can import these constants by feeding arguments to your constant class.
 
 =head1 AUTHOR
 
@@ -99,5 +153,7 @@ This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
 =head1 SEE ALSO
+
+L<Exporter>
 
 =cut
--- t/basic.t
+++
@@ -1,8 +0,0 @@
-use strict;
-use Test::More;
-use Constant::Exporter;
-
-# replace with the actual test
-ok 1;
-
-done_testing;
